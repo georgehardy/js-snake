0411f1a20fedcce7e9cdc22d718016d6f886cb83
georgehardy <george.hardy@gmail.com>
2018-07-31 18:18:35 +1200

    setup snake/canvas

--- snake.js
+++ snake.js
@@ -1,11 +1,24 @@
 document.addEventListener("DOMContentLoaded", showTitle);
 
+let canvas, ctx;
+let snakeLength, snakeSpeed;
+let gameWidth, gameHeight, gameScore;
+
+function setup() {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  gameWidth = 600;
+  gameHeight = 600;
+  gameScore = 0;
+  snakeLength = 200;
+  snakeSpeed = 1;
+}
+
 function showTitle () {
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
+  setup();
   ctx.font = "60px Tahoma";
   ctx.strokeText("SNAKE", 150, 250);
   ctx.fillStyle = "green";
-  ctx.fillRect(300, 300, 100, 20);
+  ctx.fillRect(300, 300, snakeLength, 20);
 }
 
