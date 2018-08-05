282f025c6c8b660b0d39d11d11bbcb169130440e
georgehardy <george.hardy@gmail.com>
2018-08-05 16:02:10 +1200

    Add settings. Powerup working

--- snake.js
+++ snake.js
@@ -2,31 +2,75 @@ document.addEventListener("DOMContentLoaded", showTitle);
 document.addEventListener("keydown", keyDownHandler, false);
 
 let canvas, ctx;
+let difficulty, walls, framerate;
+let gameInProgress;
+let gameScore;
+
+let powerupTimerActive = false;
+let powerupInterval = 5000;
+let powerupTime = 800;
+let powerupValue;
+
 let snakeLength, snakeSpeed;
 let snakeArray;
+let offScreen = {x:-50,y:-50}
 let applePos = [];
-let powerupPos = [];
-let gameWidth, gameHeight, gameScore;
-let direction;
-let ultiCount, ultiActive;
-let walls;
-let powerTime;
+let powerupPos = offScreen;
 
-let gameTimer = setInterval(function () { gameLoop(); }, 1000 / 60);
-let powerTimer = setInterval(function () { powerTime--; }, 200);
+let gameWidth, gameHeight;
+let direction;
+//let ultiCount, ultiActive;
+
+
+
+let gameTimer;
+let drawTimer;
+let powerTimer;
+
+
+function showTitle() {
+  gameInProgress = false;
+  settings();
+  setup();
+  ctx.font = "60px Tahoma";
+  ctx.textAlign = "center";  
+  ctx.strokeText("SNAKE", gameWidth/2, 100);
+  ctx.font = "30px Tahoma";
+  ctx.strokeText("Press space to play", gameWidth/2, 200);
+}
+
+function settings() {
+  difficulty = document.getElementById("difficulty").value;
+  walls = document.getElementById("walls").value;
+  framerate = document.getElementById("framerate").value;
+  document.getElementById("canvas").classList = "dashed";
+  if (walls == "true") { document.getElementById("canvas").classList = "solid"; }
+}
+
+function startGame(){
+  gameTimer = setInterval(function () { gameLoop(); }, 1000 / difficulty);
+  drawTimer = setInterval(function () { drawLoop(); }, 1000 / framerate);
+  
+  gameInProgress = true;
+}
+
+function gameOver() {
+  clearInterval(gameTimer);
+  clearInterval(drawTimer);
+  clearInterval(powerTimer);
+  showTitle();
+}
 
 function setup() {
+  
   gameWidth = 500;
   gameHeight = 400;
   blockSize = 20;
   gameScore = 0;
-  snakeLength = 200;
-  snakeSpeed = 1;
   snakeArray = [];
   direction = "r";
-  walls = false;
-  powerTime = 20;
-  clearInterval(powerTimer);
+  powerupValue = 0;
+  powerupTimerActive = false;
 
   snakeArray.push({ x: 200, y: 200 });
   snakeArray.push({ x: 180, y: 200 });
@@ -39,12 +83,6 @@ function setup() {
   applePos = getRandomBlock();
 }
 
-function showTitle() {
-  setup();
-  ctx.font = "60px Tahoma";
-  ctx.strokeText("SNAKE", 150, 250);
-  ctx.fillStyle = "green";
-}
 
 function drawSnake() {
   ctx.fillStyle = "#2b8a0e";
@@ -79,23 +117,49 @@ function drawPowerup() {
 }
 
 function spawnPowerup() {
-  if (Math.floor((Math.random() * 100) + 1) > 98) powerupPos = getRandomBlock();
+  clearInterval(powerTimer);
+  powerupPos = getRandomBlock();
+  powerupValue = powerupTime;
 }
 
+function activatePowerupTimer () {
+  powerTimer = setInterval(function () { spawnPowerup(); }, powerupInterval);
+  powerupPos = offScreen;
+  powerupTimerActive = true;
+}
+
+function updatePowerup() {
+  if (powerupValue > 0) powerupValue -= 20;
+  if (powerupValue == 0) removePowerup();
+}
+
+function removePowerup() {
+  powerupPos = offScreen;
+  powerupValue = 0;
+  powerupTimerActive = false;
+
+}
+
+function checkPowerup() {
+  if (!powerupTimerActive) activatePowerupTimer();
+  if (powerupTimerActive && powerupValue > 0) updatePowerup();
+}
 function gameLoop() {
   //check dot collision
   //randomly generate bonus pickup
   //draw snake movement
   //check for wrap
 
-  ctx.clearRect(0, 0, gameWidth, gameHeight);
   let removeTail = true;
   let head = { x: snakeArray[0].x, y: snakeArray[0].y };
 
+
+  checkPowerup();
+  
   switch (checkCollision(head.x, head.y)) {
     case "body":
       console.log("eatsnake");
-      showTitle();
+      gameOver();
       break;
     case "apple":
       gameScore += 5;
@@ -103,7 +167,8 @@ function gameLoop() {
       removeTail = false;
       break;
     case "powerup":
-      console.log("powerup");
+      gameScore += 50;
+      removePowerup();
       removeTail = false;
       break;
   }
@@ -123,8 +188,8 @@ function gameLoop() {
       break;
   }
 
-  if (walls) {
-    if (head.x > gameWidth || head.x < 0 || head.y > gameHeight || head.y < 0) showTitle();
+  if (walls == "true") {
+    if (head.x > gameWidth || head.x < 0 || head.y > gameHeight || head.y < 0) gameOver();
   }
   else {
     if (head.x > gameWidth - blockSize) head.x = 0;
@@ -137,14 +202,23 @@ function gameLoop() {
   if (removeTail) snakeArray.pop();
 
   
-
-  spawnPowerup();
+}
+function drawLoop() {
+  ctx.clearRect(0, 0, gameWidth, gameHeight);
+  
   drawSnake();
   drawApple();
   drawPowerup();
-  console.log(powerTime);
+  drawScore();
+  //console.log(powerTime);
 
 
+}
+
+function drawScore() {
+  document.getElementById("score").innerHTML = gameScore;
+  if (powerupValue > 0) document.getElementById("powerup").innerHTML = powerupValue;
+  else document.getElementById("powerup").innerHTML = "ff";
 }
 
 
@@ -175,7 +249,7 @@ function checkCollision(x, y) {
 }
 
 function keyDownHandler(e) {
-  console.log(e.which || e.keyCode);
+  //console.log(e.which || e.keyCode);
   if (e.keyCode == 38 || e.keyCode == 87) {
     if (direction != "d") direction = "u";
   }
@@ -191,5 +265,8 @@ function keyDownHandler(e) {
   if (e.keyCode == 113) {
     // ultiActivate(); 
   }
-  console.log(direction);
+  if (e.keyCode == 32) {
+    if(!gameInProgress) startGame(); 
+  }
+  //console.log(direction);
 }
