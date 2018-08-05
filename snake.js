ec1c2d754c9b93abed003a810813ecd3baf32a54
georgehardy <george.hardy@gmail.com>
2018-08-05 19:46:28 +1200

    first refactor

--- snake.js
+++ snake.js
@@ -22,18 +22,14 @@ let powerupPos = offScreen;
 
 let gameWidth = 500;
 let gameHeight = 400;
+let blockSize = 20;
 let direction;
-//let ultiCount, ultiActive;
-
-
 
 let gameTimer;
 let drawTimer;
 let powerTimer;
-let ultiTimer;
 
 function loadCanvas () {
-  
   canvas = document.getElementById("canvas");
   canvas.setAttribute('width', gameWidth);
   canvas.setAttribute('height', gameHeight);
@@ -41,12 +37,38 @@ function loadCanvas () {
   document.fonts.load('10pt "Russo One"').then(showTitle);
 }
 
+function reset() {
+  document.getElementById("settings").classList = "";
+
+  gameInProgress = false;
+  powerupTimerActive = false;
+  ultimateActive = false;
+
+  gameScore = 0;
+  powerupValue = 0;
+  powerupCharge = 0;
+  snakeArray = [];
+  direction = "r";
+  
+  snakeArray.push({ x: 200, y: 200 });
+  snakeArray.push({ x: 180, y: 200 });
+  applePos = getRandomBlock();
+}
+
+function settings() {
+  difficulty = document.getElementById("difficulty").value;
+  walls = document.getElementById("walls").value;
+  framerate = document.getElementById("framerate").value;
+  document.getElementById("canvas").classList = "dashed";
+  if (walls == "true") { document.getElementById("canvas").classList = "solid"; }
+}
+
 function showTitle() {
-  setup();
+  reset();
+  
   ctx.font = "100px Russo One";
   ctx.textAlign = "center";  
   ctx.strokeText("SNAKE", gameWidth/2, 125);
-
   ctx.font = "22px Russo One";
   ctx.fillText("Use arrows or WASD to move", gameWidth/2, 225);
   ctx.fillText("Press Q to activate ultimate", gameWidth/2, 275);
@@ -54,13 +76,27 @@ function showTitle() {
   ctx.strokeText("Press space to start", gameWidth/2, 350);
 }
 
-function settings() {
-  console.log("test");
-  difficulty = document.getElementById("difficulty").value;
-  walls = document.getElementById("walls").value;
-  framerate = document.getElementById("framerate").value;
-  document.getElementById("canvas").classList = "dashed";
-  if (walls == "true") { document.getElementById("canvas").classList = "solid"; }
+function keyDownHandler(e) {
+  //console.log(e.which || e.keyCode);
+  if (e.keyCode == 38 || e.keyCode == 87) {
+    if (direction != "d") direction = "u";
+  }
+  if (e.keyCode == 40 || e.keyCode == 83) {
+    if (direction != "u") direction = "d";
+  }
+  if (e.keyCode == 37 || e.keyCode == 65) {
+    if (direction != "r") direction = "l";
+  }
+  if (e.keyCode == 39 || e.keyCode == 68) {
+    if (direction != "l") direction = "r";
+  }
+  if (e.keyCode == 81) {
+    if (powerupCharge == 100) ultimateActive = true; 
+  }
+  if (e.keyCode == 32) {
+    if(!gameInProgress) startGame(); 
+  }
+  //console.log(direction);
 }
 
 function startGame(){
@@ -85,105 +121,12 @@ function gameOver() {
   ctx.fillText("High Score: " + highScore, gameWidth/2, 250);
   ctx.font = "35px Russo One";
   ctx.strokeText("Press space to play again", gameWidth/2, 350);
-  setup();
+  reset();
 }
-
-function setup() {
-  document.getElementById("settings").classList = "";
-  gameInProgress = false;
-  gameWidth = 500;
-  gameHeight = 400;
-  blockSize = 20;
-  gameScore = 0;
-  snakeArray = [];
-  direction = "l";
-  powerupValue = 0;
-  powerupTimerActive = false;
-  powerupCharge = 0;
-  ultimateActive = false;
-
-  snakeArray.push({ x: 200, y: 200 });
-  snakeArray.push({ x: 180, y: 200 });
-
-
-  applePos = getRandomBlock();
-}
-
-
-function drawSnake() {
-  ctx.fillStyle = "#2b8a0e";
-  if (ultimateActive) ctx.fillStyle = "#d89b00";
-  ctx.fillRect(snakeArray[0].x, snakeArray[0].y, blockSize, blockSize);
-  ctx.fillStyle = "#3fb41c";
-  if (ultimateActive) ctx.fillStyle = "#ffb800";
-  for (let i = 1; i < snakeArray.length; i++) {
-    ctx.fillRect(snakeArray[i].x, snakeArray[i].y, blockSize, blockSize);
-  }
-}
-
-function drawApple() {
-
-  ctx.beginPath();
-  ctx.arc(applePos.x + (blockSize / 2), applePos.y + (blockSize / 2), blockSize / 4, 0, 2 * Math.PI);
-  ctx.fillStyle = "red";
-  ctx.fill();
-  ctx.lineWidth=0.5;
-  ctx.stroke();
-}
-
-function drawPowerup() {
-  
-  ctx.save();
-  ctx.fillStyle = "#f72bf7";
-  ctx.translate(powerupPos.x + (blockSize / 2), powerupPos.y + (blockSize / 2));
-  ctx.rotate(Math.PI / 4);
-  ctx.scale(0.5,0.5)
-  ctx.fillRect(-blockSize/2, -blockSize/2, blockSize, blockSize);
-  ctx.strokeRect(-blockSize/2, -blockSize/2, blockSize, blockSize);
-
-  ctx.restore();
-}
-
-function spawnPowerup() {
-  clearInterval(powerTimer);
-  powerupPos = getRandomBlock();
-  powerupValue = powerupTime;
-}
-
-function activatePowerupTimer () {
-  powerTimer = setInterval(function () { spawnPowerup(); }, powerupInterval);
-  powerupPos = offScreen;
-  powerupTimerActive = true;
-}
-
-function updatePowerup() {
-  if (powerupValue > 0) powerupValue -= 20;
-  if (powerupValue == 0) removePowerup();
-}
-
-function removePowerup() {
-  powerupPos = offScreen;
-  powerupValue = 0;
-  powerupTimerActive = false;
-
-}
-
-function checkPowerup() {
-  if (!powerupTimerActive) activatePowerupTimer();
-  if (powerupTimerActive && powerupValue > 0) updatePowerup();
-}
-
 
 function gameLoop() {
-  //check dot collision
-  //randomly generate bonus pickup
-  //draw snake movement
-  //check for wrap
-
   let removeTail = true;
-  head = { x: snakeArray[0].x, y: snakeArray[0].y };
-
-
+  let head = { x: snakeArray[0].x, y: snakeArray[0].y };
   checkPowerup();
 
   if (ultimateActive && powerupCharge > 0) powerupCharge -= 1;
@@ -237,36 +180,7 @@ function gameLoop() {
 
   snakeArray.unshift(head);
   if (removeTail) snakeArray.pop();
-
-  
 }
-function drawLoop() {
-  ctx.clearRect(0, 0, gameWidth, gameHeight);
-  
-  drawSnake();
-  drawApple();
-  drawPowerup();
-  drawHUD();
-  
-  //console.log(powerTime);
-
-
-}
-
-function drawHUD() {
-  ctx.fillStyle= "#0b4b35";
-  ctx.font = "15px Trebuchet MS";
-  ctx.textAlign = "left";  
-  ctx.fillText(gameScore, 10, 20);
-  ctx.textAlign = "right";
-  if (powerupValue > 0) ctx.fillText(powerupValue, gameWidth - 10, 20);
-  ctx.textAlign = "left";
-  if (powerupCharge == 100) ctx.fillStyle= "red";
-  ctx.fillText(powerupCharge+"%", 10, gameHeight - 10);
-
-}
-
-
 
 function getRandomBlock() {
   let randX = blockSize * (Math.floor(Math.random() * (gameWidth - blockSize) / blockSize));
@@ -293,25 +207,84 @@ function checkCollision(x, y) {
   return null;
 }
 
-function keyDownHandler(e) {
-  console.log(e.which || e.keyCode);
-  if (e.keyCode == 38 || e.keyCode == 87) {
-    if (direction != "d") direction = "u";
+function spawnPowerup() {
+  clearInterval(powerTimer);
+  powerupPos = getRandomBlock();
+  powerupValue = powerupTime;
+}
+
+function activatePowerupTimer () {
+  powerTimer = setInterval(function () { spawnPowerup(); }, powerupInterval);
+  powerupPos = offScreen;
+  powerupTimerActive = true;
+}
+
+function updatePowerup() {
+  if (powerupValue > 0) powerupValue -= 20;
+  if (powerupValue == 0) removePowerup();
+}
+
+function removePowerup() {
+  powerupPos = offScreen;
+  powerupValue = 0;
+  powerupTimerActive = false;
+
+}
+
+function checkPowerup() {
+  if (!powerupTimerActive) activatePowerupTimer();
+  if (powerupTimerActive && powerupValue > 0) updatePowerup();
+}
+
+function drawLoop() {
+  ctx.clearRect(0, 0, gameWidth, gameHeight);
+  drawSnake();
+  drawApple();
+  drawPowerup();
+  drawHUD();
+}
+
+function drawSnake() {
+  ctx.fillStyle = "#2b8a0e";
+  if (ultimateActive) ctx.fillStyle = "#d89b00";
+  ctx.fillRect(snakeArray[0].x, snakeArray[0].y, blockSize, blockSize);
+  ctx.fillStyle = "#3fb41c";
+  if (ultimateActive) ctx.fillStyle = "#ffb800";
+  for (let i = 1; i < snakeArray.length; i++) {
+    ctx.fillRect(snakeArray[i].x, snakeArray[i].y, blockSize, blockSize);
   }
-  if (e.keyCode == 40 || e.keyCode == 83) {
-    if (direction != "u") direction = "d";
-  }
-  if (e.keyCode == 37 || e.keyCode == 65) {
-    if (direction != "r") direction = "l";
-  }
-  if (e.keyCode == 39 || e.keyCode == 68) {
-    if (direction != "l") direction = "r";
-  }
-  if (e.keyCode == 81) {
-    if (powerupCharge == 100) ultimateActive = true; 
-  }
-  if (e.keyCode == 32) {
-    if(!gameInProgress) startGame(); 
-  }
-  //console.log(direction);
+}
+
+function drawApple() {
+
+  ctx.beginPath();
+  ctx.arc(applePos.x + (blockSize / 2), applePos.y + (blockSize / 2), blockSize / 4, 0, 2 * Math.PI);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.lineWidth=0.5;
+  ctx.stroke();
+}
+
+function drawPowerup() {
+  ctx.save();
+  ctx.fillStyle = "#f72bf7";
+  ctx.translate(powerupPos.x + (blockSize / 2), powerupPos.y + (blockSize / 2));
+  ctx.rotate(Math.PI / 4);
+  ctx.scale(0.5,0.5)
+  ctx.fillRect(-blockSize/2, -blockSize/2, blockSize, blockSize);
+  ctx.strokeRect(-blockSize/2, -blockSize/2, blockSize, blockSize);
+  ctx.restore();
+}
+
+function drawHUD() {
+  ctx.fillStyle= "#0b4b35";
+  ctx.font = "15px Trebuchet MS";
+  ctx.textAlign = "left";  
+  ctx.fillText(gameScore, 10, 20);
+  ctx.textAlign = "right";
+  if (powerupValue > 0) ctx.fillText(powerupValue, gameWidth - 10, 20);
+  ctx.textAlign = "left";
+  if (powerupCharge == 100) ctx.fillStyle= "red";
+  ctx.fillText(powerupCharge+"%", 10, gameHeight - 10);
+
 }
